cdd814281ee67fe24fafcf1cd894cbae85261024
Deepak Shenoy <shenoyd@jupiter.local>
2024-09-05 08:39:45 -0400

    initial structure

---
+++ src/app/components/create-case/create-case.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-case',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-case.component.html',
+  styleUrl: './create-case.component.css'
+})
+export class CreateCase {
+
+}
