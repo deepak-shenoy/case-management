8ffcb269eeab27f305a1ae764d7d8a76763c1103
Deepak Shenoy <shenoyd@jupiter.local>
2024-09-06 12:52:06 -0400

    Updating application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,19 @@
  */
 
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatMenuModule,
+    MatButtonModule,
+    RouterLink,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
